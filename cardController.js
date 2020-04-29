aaa3f26fa923a14216eb94827378f5a8268ce16c
Adrian Li <adrian.li@freshup.com.hk>
2020-04-29 12:09:18 +0800

    added card image (small size).

--- cardController.js
+++ cardController.js
@@ -4,6 +4,9 @@ var cardController = {};
 
 cardController.getCard = (req, res) => {
   let card = cardData['data'].find(card => card.info_1 === req.params.cardId);
+  Object.assign(card, {
+    card_image_small: 'http://hk.battlespirits.com/image/card_serch/card/' + card.info_1 + '.jpg'
+  });
   res.json(card);
 };
 
